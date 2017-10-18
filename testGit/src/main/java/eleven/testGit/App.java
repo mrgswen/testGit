9d3cec8736e88bb70237fc5700cd9e98d95072d8
mrgswen <1157387265@qq.com>
2017-10-18 18:19:53 +0800

    testgit

---
+++ testGit/src/main/java/eleven/testGit/App.java
@@ -0,0 +1,13 @@
+package eleven.testGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
